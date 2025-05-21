19da24e77b224bb076bf642586cf73b2a85700b4
Sky Software Solution <skysoftware2020@gmail.com>
2025-05-21 23:07:23 +0530

    resume fix

--- src/app/api/v1/create-resume/route.js
+++ src/app/api/v1/create-resume/route.js
@@ -1,14 +1,11 @@
-import path from 'path'
 import puppeteer from 'puppeteer';
-import fs from 'fs'
 import { sequelize } from '@/lib/db';
 import UserResume from '@/models/resume';
+import uploadOnCloudinary from '@/lib/cloudinary';
+
 
 export async function POST(req, res) {
-
   const { formData } = await req.json()
-
-  
 
   try {
     await sequelize.authenticate()
@@ -16,59 +13,34 @@ export async function POST(req, res) {
 
     await UserResume.create({ ...formData })
 
-    const folderPath = path.resolve(process.cwd(), "public", "resume")
 
-  if (!fs.existsSync(folderPath)) {
-    fs.mkdirSync(folderPath, { recursive: true });
-  }
 
-  const filename = formData.fullName.split(" ").join("-").toLowerCase()
-
-  const filePath = path.join(
-    folderPath, `${filename}.pdf`
-  )
-
-  const browser = await puppeteer.launch({
-    headless: "new",
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
-  })
-
-  const resumeHtml = generateResumeHTML(formData)
-
-  const page = await browser.newPage();
-  await page.setContent(resumeHtml, { waitUntil: "networkidle0" })
-
-  const pdfBuffer = await page.pdf({ format: "A4" })
-
-  fs.writeFileSync(filePath, pdfBuffer)
-
-  const deletionTimeout = 5 * 60 * 1000
-
-  const time = setTimeout(() => {
-    fs.unlink(filePath, (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log("File deleted successfully.");
-        clearTimeout(time)
-      }
+    const browser = await puppeteer.launch({
+      headless: "new",
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
     })
-  }, deletionTimeout)
 
-  return Response.json({
-    message: "Resume generated Successfully.",
-  })
-  } catch (err) {
-    console.log(err);
-    
+    const resumeHtml = generateResumeHTML(formData)
+
+    const page = await browser.newPage();
+    await page.setContent(resumeHtml, { waitUntil: "networkidle0" })
+
+    const pdfBuffer = await page.pdf({ format: "A4" })
+
+    const cloudinaryUrl = await uploadOnCloudinary(pdfBuffer, formData.fullName.split(" ").join("-").toLowerCase())
+
     return Response.json({
-    message: err.message,
-  })
+      message: "Resume generated Successfully.",
+      url: cloudinaryUrl.secure_url
+    })
+  } catch (err) {
+    return Response.json({
+      message: err.message,
+    })
   }
 
-  
-}
 
+}
 
 function generateResumeHTML(formData) {
   return `
@@ -126,7 +98,7 @@ function generateResumeHTML(formData) {
         >
           <img
             src=${formData.profileImage}
-            width="150"
+            width="150px"
             style="object-fit: contain"
           />
         </div>
