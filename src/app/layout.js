dcd73931edf5a62bbfef77ed6e7bb32e1ea97879
sky-software-solution-2020 <skysoftware2020@gmail.com>
2025-06-01 10:38:20 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import "./globals.css";
 export const metadata = {
   title: "Sky Software Solution",
   description: "Sky Software Solution provides IT training at Neemuch, specializing in Web Development Training and the best coding classes at Neemuch!",
+  keyword:"python classess in neemuch,c and c++ coaching in neemuch, c classes  in neemuch, programming classess in neemuch ,most reviewed class in Neemuch,it sector courses,,best placement coaching in Neemuch,software training institute in Neemuch,best software training institute in Neemuch,training and placement in Neemuch,software training and placement,best coding classes in Neemuch"
 };
 
 export default function RootLayout({ children }) {
