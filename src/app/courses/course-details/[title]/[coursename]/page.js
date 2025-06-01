4beb21d2b8d69bb817f34f3a46ea825dc27c209b
Sky Software Solution <skysoftware2020@gmail.com>
2025-06-01 12:08:54 +0530

    remove metadata

--- src/app/courses/course-details/[title]/[coursename]/page.js
+++ src/app/courses/course-details/[title]/[coursename]/page.js
@@ -3,32 +3,32 @@
 import CoursesDetailPage from "@/components/coursedetails/coursedetailpage";
 
 
-export async function generateMetadata({ params }) {
-  const { coursename, title } = await params
+// export async function generateMetadata({ params }) {
+//   const { coursename, title } = await params
 
 
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
-   const decodedCourseName = decodeURIComponent(coursename);
-  const res = await fetch(`${baseUrl}/api/v1/courses`);
-  const courses = await res.json();
-  const course = courses.find(c => c.courseName === decodedCourseName);
+//   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
+//    const decodedCourseName = decodeURIComponent(coursename);
+//   const res = await fetch(`${baseUrl}/api/v1/courses`);
+//   const courses = await res.json();
+//   const course = courses.find(c => c.courseName === decodedCourseName);
 
-  return {
-    title: `${title.replace(title[0], title[0].toUpperCase())} - ${decodedCourseName.replace(decodedCourseName[0], decodedCourseName[0].toUpperCase())}`,
-    description: course.overview.description,
-    openGraph: {
-      title: `${title.replace(title[0], title[0].toUpperCase())} - ${decodedCourseName.replace(decodedCourseName[0], decodedCourseName[0].toUpperCase())}`,
-      description: course.overview.description,
-      images: [course.avatarImage, ...course.coverImages],
+//   return {
+//     title: `${title.replace(title[0], title[0].toUpperCase())} - ${decodedCourseName.replace(decodedCourseName[0], decodedCourseName[0].toUpperCase())}`,
+//     description: course.overview.description,
+//     openGraph: {
+//       title: `${title.replace(title[0], title[0].toUpperCase())} - ${decodedCourseName.replace(decodedCourseName[0], decodedCourseName[0].toUpperCase())}`,
+//       description: course.overview.description,
+//       images: [course.avatarImage, ...course.coverImages],
 
-    },
-    icons: {
-      icon: course.avatarImage,
-      shortcut: course.avatarImage
-    }
-  }
+//     },
+//     icons: {
+//       icon: course.avatarImage,
+//       shortcut: course.avatarImage
+//     }
+//   }
 
-}
+// }
 
 
 export default async function CoursesDetails({ params }) {
