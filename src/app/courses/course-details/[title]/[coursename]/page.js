a7a73b0e8eadecfe24fd564c558b6a1bd3203e03
Sky Software Solution <skysoftware2020@gmail.com>
2025-06-01 11:50:55 +0530

    bug fixes

--- src/app/courses/course-details/[title]/[coursename]/page.js
+++ src/app/courses/course-details/[title]/[coursename]/page.js
@@ -1,7 +1,6 @@
 "use server"
 
 import CoursesDetailPage from "@/components/coursedetails/coursedetailpage";
-import axios from "axios";
 
 
 export async function generateMetadata({ params }) {
@@ -10,11 +9,9 @@ export async function generateMetadata({ params }) {
 
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
 
-  const courses = await axios.get(
-    `${baseUrl}/api/v1/courses`
-  );
-
-  const course = courses.data.find(c => c.courseName === coursename)
+  const res = await fetch(`${baseUrl}/api/v1/courses`);
+const courses = await res.json();
+const course = courses.find(c => c.courseName === coursename);
 
   return {
     title: `${title.replace(title[0], title[0].toUpperCase())} - ${coursename.replace(coursename[0], coursename[0].toUpperCase())}`,
